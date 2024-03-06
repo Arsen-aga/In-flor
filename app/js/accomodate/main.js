c9fcfb7d4c9520082c4a270ae7969e53e82ca67b
Arsen Asanov <tatarin08082002@gmail.com>
2024-03-06 16:52:47 +0300

    added seven blocks

--- app/js/accomodate/main.js
+++ app/js/accomodate/main.js
@@ -1,2 +1,13 @@
-//= ../components/modal.js
-console.log('Hello');
+//= ../libs/swiper-bundle.min.js
+
+$(".services__slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+});
+$(".reviews__slider").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+});
+	
