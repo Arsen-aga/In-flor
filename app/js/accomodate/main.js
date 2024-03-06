3497cad4ccb70ca033897bad1fabd6e1689ed2ff
Arsen Asanov <tatarin08082002@gmail.com>
2024-03-07 02:33:31 +0300

    added all blocks

--- app/js/accomodate/main.js
+++ app/js/accomodate/main.js
@@ -10,4 +10,10 @@ $(".reviews__slider").slick({
   slidesToShow: 4,
   slidesToScroll: 1,
 });
-	
+
+if ($(".questions__item-top")) {
+  $(".questions__item-top").on('click', function(){
+    $(".questions__item-top").removeClass("active");
+    $(this).toggleClass('active');
+  })
+}
