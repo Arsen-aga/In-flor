74998e668d9ac6d6233d99b6e8402691fba6efbe
Arsen Asanov <tatarin08082002@gmail.com>
2024-03-08 03:54:04 +0300

    All blocks added and adapted

--- app/js/accomodate/main.js
+++ app/js/accomodate/main.js
@@ -1,19 +1,78 @@
-//= ../libs/swiper-bundle.min.js
+//= ../libs/slick.min.js
+//= ../libs/fancybox.umd.js
 
 $(".services__slider").slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 568,
+      settings: {
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
 $(".reviews__slider").slick({
-  infinite: true,
+  infinite: false,
   slidesToShow: 4,
   slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 3,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 568,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 421,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+        dots: true,
+      },
+    },
+  ],
 });
 
-if ($(".questions__item-top")) {
-  $(".questions__item-top").on('click', function(){
-    $(".questions__item-top").removeClass("active");
-    $(this).toggleClass('active');
-  })
+if ($(".questions__item")) {
+  $(".questions__item").on("click", function () {
+    $(".questions__item").removeClass("active");
+    $(this).toggleClass("active");
+  });
 }
+
+Fancybox.bind("[data-fancybox]", {});
